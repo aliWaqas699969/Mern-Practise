40898ae81e78b8ced5e8c1424a0dd3d55abda184
Ali waqas <aliwaqas55488@gmail.com>
2025-08-05 12:43:28 +0500

    Updated Pages

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -2,7 +2,22 @@ import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import React from "react";
 import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
-function NoteCard({ note }) {
+import api from "../lib/axios";
+import toast from "react-hot-toast";
+function NoteCard({ note,setNotes  }) {
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+    if (!window.confirm("Are you sure you want to delete this note?"));
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prevNotes) => prevNotes.filter((note) => note._id !== id));
+      toast.success("Note deleted successfully");
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to delete note");
+    }
+  };
+
   return (
     <Link
       to={`/note/${note._id}`}
@@ -18,7 +33,10 @@ function NoteCard({ note }) {
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
 
-            <button className="btn btn-ghost btn-xs text-error">
+            <button
+              onClick={(e) => handleDelete(e, note._id)}
+              className="btn btn-ghost btn-xs text-error"
+            >
               <Trash2Icon className="size-4" />
             </button>
           </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -39,11 +39,13 @@ function HomePage() {
         {loading && (
           <div className="text-center text-primary py-10">Loading Notes...</div>
         )}
-
+        {notes.length === 0 && !loading && (
+          <div className="text-center text-primary py-10">No notes found</div>
+        )}
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
